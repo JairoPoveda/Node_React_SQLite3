ba0949d26a314fc7ea028bc29b724673d59d4ffa
jairopoveda <jairopoveda2022@hotmail.com>
2022-12-16 01:34:38 +0800

    first commit

---
+++ frontend/src/modules/dashboard/dashboardService.tsx
@@ -0,0 +1,15 @@
+import authAxios from 'src/modules/shared/axios/authAxios';
+import AuthCurrentTenant from 'src/modules/auth/authCurrentTenant';
+
+export default class DashboardService {
+  static async fetchDashboardData() {
+    const tenantId = AuthCurrentTenant.get();
+
+    const response = await authAxios.get(
+      `/tenant/${tenantId}/dashboard`
+      //`/tenant/${tenantId}/job-title`,
+    );
+
+    return response.data;
+  }
+}
